292b3ebcf062f37d7a44f3a2eceaaf465323e9f5
amabelle <amabelletr@gmail.com>
2021-04-26 04:02:09 +0300

    Routing and linking menu-item navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import HatsPage from './pages/HatsPage';
+
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/hats">
+            <HatsPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/HatsPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HatsPage = () => {
+  return <div>HATS</div>;
+};
+
+export default HatsPage;
