07e93954f47fceca241d966ea0755d546f0d4e75
amabelle <amabelletr@gmail.com>
2021-06-23 14:12:48 +0300

    dispatch shorthand for hidden cart

--- src/components/cart-dropdown/index.js
+++ src/components/cart-dropdown/index.js
@@ -6,10 +6,11 @@ import { createStructuredSelector } from 'reselect';
 import CustomButton from '../custom-button';
 import CartItem from '../cart-item';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => {
+const CartDropdown = ({ cartItems, dispatch  }) => {
   const history = useHistory();
 return (
   <div className='cart-dropdown'>
@@ -23,7 +24,10 @@ return (
         <span className='empty-message'>Your cart is empty</span>
     }
     </div>
-    <CustomButton onClick={history.push('./checkout')}>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick={() => {
+      history.push('./checkout')
+      dispatch(toggleCartHidden())
+    }}>GO TO CHECKOUT</CustomButton>
   </div>
 );
 }
